d27d3bd1e5e6968bb086755951636b5a63b82336
whjin <whjin@grandstream.cn>
2017-02-14 15:33:55 +0800

    [NBF] add SnackBar Cause: None Solution: None Fixed Version: 2017/02/14 15:34 Branch: master

--- MaterialDesign/app/src/main/java/whjin/example/com/materialdesign/MainActivity.java
+++ MaterialDesign/app/src/main/java/whjin/example/com/materialdesign/MainActivity.java
@@ -3,6 +3,7 @@ package whjin.example.com.materialdesign;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.NavigationView;
+import android.support.design.widget.Snackbar;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
@@ -91,7 +92,15 @@ public class MainActivity extends AppCompatActivity{
         floatingActionButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(MainActivity.this, "click floatingActionButton", Toast.LENGTH_SHORT).show();
+//                Toast.makeText(MainActivity.this, "click floatingActionButton", Toast.LENGTH_SHORT).show();
+                Snackbar.make(v, "Data delete", Snackbar.LENGTH_LONG)
+                        .setAction("Undo", new View.OnClickListener() {
+                            @Override
+                            public void onClick(View v) {
+                                Toast.makeText(MainActivity.this, "Data restore", Toast.LENGTH_SHORT).show();
+                            }
+                        })
+                        .show();
             }
         });
 
